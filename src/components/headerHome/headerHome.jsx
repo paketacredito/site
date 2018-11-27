9da2ae4217e55ecca00a0e500f2276ccd18e0efc
Eduarda Scharnhorst <eduardascharnhorst@gmail.com>
2018-11-27 01:40:45 -0200

    Create navbar component

---
+++ src/components/headerHome/headerHome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../navbar/navbar'
+
+import './headerHome.css'
+
+const HeaderHome = () => (
+  <div className="header--home">
+    <Navbar />
+    <p>Header</p>
+  </div>
+)
+
+export default HeaderHome
---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Header from '../components/headerHome/headerHome'
+
+import '../components/layout.css'
+
+const IndexPage = () => (
+  <div className="home">
+    <Header />
+  </div>
+)
+
+export default IndexPage

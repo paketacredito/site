d9b63f6e14a32bb52eed2a1de0d4919520bc6da5
Eduarda Scharnhorst <eduardascharnhorst@gmail.com>
2018-11-27 22:35:59 -0200

    new section at home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,54 @@
 import React from 'react'
 
 import Header from '../components/headerHome/headerHome'
+import Card from '../components/card/card'
+
+import JurosBaixos from '../images/icon-paketa-jurosbaixos.svg'
+import ParcelasFixas from '../images/icon-paketa-parcelasfixas.svg'
+import Credito from '../images/icon-paketa-credito.svg'
+import DescontoFolha from '../images/icon-paketa-descotonafolha.svg'
+
 
 import '../components/layout.css'
+import './index.css'
 
 const IndexPage = () => (
   <div className="home">
     <Header />
+
+    <section id="credit" className="home__credit">
+      <h3>
+        Crédito Consignado para funcionários de
+        empresas conveniadas
+      </h3>
+
+      <div className="credit__card">
+        <Card
+          src={JurosBaixos}
+          alt="Juros Baixos"
+          text="Taxas de juros mais baixas que outras linhas de crédito"
+        />
+
+        <Card
+          src={ParcelasFixas}
+          alt="Parcelas Fixas"
+          text="Parcelas fixas de até 30% do salário líquido"
+        />
+
+        <Card
+          src={Credito}
+          alt="Crédito"
+          text="Crédito pode ser usado para qualquer finalidade"
+        />
+
+        <Card
+          src={DescontoFolha}
+          alt="Desconto na folha"
+          text="Desconto das parcelas na folha de pagamento"
+        />
+
+      </div>
+    </section>
   </div>
 )
 

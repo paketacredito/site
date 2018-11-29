dac8726166f219931a748b130bc885c4d5ba5e86
Eduarda Scharnhorst <eduardascharnhorst@gmail.com>
2018-11-28 23:07:08 -0200

    header component

---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../navbar/navbar'
+
+import './header.css'
+
+const Header = () => (
+  <div className="header">
+    <Navbar />
+    <p>
+      Crédito consignado feito de uma forma diferente.
+      Praticidade e eficiência para a sua empresa.
+    </p>
+  </div>
+)
+
+export default Header
---
+++ src/pages/quem-somos.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Header from '../components/header/header'
+
+const About = () => (
+  <div className="about">
+    <Header />
+    
+  </div>
+)
+
+export default About
